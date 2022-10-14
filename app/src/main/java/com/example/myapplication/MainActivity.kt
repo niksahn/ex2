13df2ad954aa8cdd344b069b9cb50064980facba
niksahn <niksahn@gmail.com>
2022-10-14 16:24:08 +0300

    Toolbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,6 +14,7 @@ import java.io.ByteArrayOutputStream
 class MainActivity : AppCompatActivity() {
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
+    private lateinit var title: TextView
     private lateinit var imageView1: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun click() {
+    fun click(view: View) {
+
+
         val bitmap = (imageView1.getDrawable() as BitmapDrawable).bitmap
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
@@ -32,42 +35,47 @@ class MainActivity : AppCompatActivity() {
         secondActi.putExtra(MainActivity2.text1, text1)
         secondActi.putExtra(MainActivity2.text2, text2)
         secondActi.putExtra("picture", byteArray)
+        try {
+            //if (view.id!=null and view.id )
+          title= findViewById(view.id)
+          secondActi.putExtra(MainActivity2.titlee, title.text)
+
+        }catch (e:Exception){}
         startActivity(secondActi)
     }
 
-    fun click5(view: View,textView: TextView) {
-        print(view.getId())
-        textView1 = findViewById(textView.getId())
-        textView2 = findViewById(textView.getId())
+    fun click5(view: View) {
+        textView1 = findViewById(R.id.textView5)
+        textView2 = findViewById(R.id.textView5_1)
         imageView1 = findViewById(R.id.imageView5)
-        click()
+        click(view)
     }
 
     fun click4(view: View) {
         textView1 = findViewById(R.id.textView4)
         textView2 = findViewById(R.id.textView4_1)
         imageView1 = findViewById(R.id.imageView4)
-        click()
+        click(view)
     }
 
     fun click3(view: View) {
         textView1 = findViewById(R.id.textView3)
         textView2 = findViewById(R.id.textView3_1)
         imageView1 = findViewById(R.id.imageView3)
-        click()
+        click(view)
     }
 
     fun click2(view: View) {
         textView1 = findViewById(R.id.textView2)
         textView2 = findViewById(R.id.textView2_1)
         imageView1 = findViewById(R.id.imageView2)
-        click()
+        click(view)
     }
 
     fun click1(view: View) {
         textView1 = findViewById(R.id.textView1)
         textView2 = findViewById(R.id.textView1_1)
         imageView1 = findViewById(R.id.imageView1)
-        click()
+        click(view)
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -13,12 +14,14 @@ class MainActivity2 : AppCompatActivity() {
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
     private lateinit var imageView1: ImageView
+    private lateinit var Title:androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         textView2 = findViewById(R.id.textView)
         textView1 = findViewById(R.id.textVw1)
         imageView1 = findViewById(R.id.imageView)
+        Title=findViewById(R.id.toolbar)
         show()
     }
 
@@ -26,6 +29,7 @@ class MainActivity2 : AppCompatActivity() {
 
         const val text1 = "text1"
         const val text2 = "text2"
+        const val titlee = "title"
 
 
     }
@@ -48,10 +52,10 @@ class MainActivity2 : AppCompatActivity() {
 
         val inttext1 = intent.getStringExtra(text1)
         val inttext2 = intent.getStringExtra(text2)
+
         textView1.text = inttext1
         textView2.text = inttext2
-
-
+        Title.title=intent.getStringExtra(titlee)
     }
 
 }
