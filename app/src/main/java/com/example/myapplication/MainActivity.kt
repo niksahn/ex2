9dfcf7134c620107f2e3ecb7cb75a25fa2e31ed4
niksahn <niksahn@gmail.com>
2022-10-15 15:23:55 +0300

    inflate

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,30 +1,56 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.ByteArrayOutputStream
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView1: TextView
+    private  var name = arrayOf(
+        "Ели","Мужики","Запивали","Конюх","Они"
+
+    )
+    private  var position = arrayOf(
+        "Мясо","Пивом","О чем","Говорил","Не понимали"
+    )
+    private   var images= arrayOf (
+        "img","img","img","img","img"
+    )
+    /*private lateinit var textView1: TextView
     private lateinit var textView2: TextView
     private lateinit var title: TextView
+    private lateinit var imageView1: ImageView*/
+    private lateinit var textView1: TextView
+    private lateinit var textView2: TextView
+    private lateinit var linLayout: LinearLayout
     private lateinit var imageView1: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar2))
+        linLayout = findViewById(R.id.linearLayout)
+        val ltInflater = layoutInflater
+        for (i in 0 until name.size) {
+            val item: View = ltInflater.inflate(R.layout.element, linLayout, false)
+            textView1 = item.findViewById(R.id.textView1)
+            textView1.text = name[i]
+            textView2 = item.findViewById(R.id.textView1_1)
+            textView2.text = position[i]
+            imageView1 = item.findViewById(R.id.imageView1)
+            val a=images[i]
+            val drawableResourceId =this.resources.getIdentifier("$a", "drawable", this.packageName)
+            imageView1.setImageResource(drawableResourceId)
+            item.getLayoutParams().width = FrameLayout.LayoutParams.MATCH_PARENT
+            linLayout.addView(item)
+        }
 
     }
 
-    fun click(view: View) {
-
-
+    /*fun click(view: View) {
         val bitmap = (imageView1.getDrawable() as BitmapDrawable).bitmap
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
@@ -78,5 +104,5 @@ class MainActivity : AppCompatActivity() {
         textView2 = findViewById(R.id.textView1_1)
         imageView1 = findViewById(R.id.imageView1)
         click(view)
-    }
+    }*/
 }
